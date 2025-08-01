6a2c880e65d2ed3258cc75552111585bf426e3c6
Fatih Koçer <fatihkocerc@gmail.com>
2025-08-01 17:03:33 +0300

    add CreateLogsTable and SQL queries, add test for CreateLogsTable

--- exporter.go
+++ exporter.go
@@ -3,10 +3,48 @@ package sqlitexporter
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
+)
+
+const (
+	CreateLogsTableSQL = `
+		CREATE TABLE IF NOT EXISTS %s (
+			ID INTEGER PRIMARY KEY AUTOINCREMENT,
+			ResourceSchemaURL TEXT,
+			ScopeSchemaURL TEXT,
+			ObservedTimestamp TEXT,
+			Timestamp TEXT,
+			SeverityNumber INTEGER,
+			SeverityText TEXT,
+			Body TEXT,
+			TraceID TEXT,
+			SpanID TEXT,
+			EventName TEXT,
+			ResourceAttrs BLOB,
+			LogAttrs BLOB
+		);
+	`
+
+	InsertLogsTableSQL = `
+		INSERT INTO %s (
+			ResourceSchemaURL,
+			ScopeSchemaURL,
+			ObservedTimestamp,
+			Timestamp,
+			SeverityNumber,
+			SeverityText,
+			Body,
+			TraceID,
+			SpanID,
+			EventName,
+			ResourceAttrs,
+			LogAttrs
+		) VALUES (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+	`
 )
 
 type logsExporter struct {
@@ -28,13 +66,12 @@ func newLogsExporter(logger *zap.Logger, cfg *Config) (*logsExporter, error) {
 	}, nil
 }
 
-// TODO: write table initializer
 func (e *logsExporter) Start(ctx context.Context, _ component.Host) error {
 	e.logger.Info("starting sqlitexporter",
 		zap.String("filename", e.cfg.DatabaseFilename),
 		zap.String("logs table", e.cfg.LogsTableName),
 	)
-	return nil
+	return CreateLogsTable(ctx, e.cfg, e.client)
 }
 
 func (e *logsExporter) Shutdown(_ context.Context) error {
@@ -49,5 +86,16 @@ func (e *logsExporter) Shutdown(_ context.Context) error {
 }
 
 func (e *logsExporter) pushLogData(ctx context.Context, record plog.Logs) error {
+	return nil
+}
+
+func CreateLogsTable(ctx context.Context, cfg *Config, client *sql.DB) error {
+	str := fmt.Sprintf(CreateLogsTableSQL, cfg.LogsTableName)
+
+	_, err := client.ExecContext(ctx, str)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- exporter_test.go
+++ exporter_test.go
@@ -1,5 +1,36 @@
 package sqlitexporter
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+)
 
 func TestExporterLogs(t *testing.T) {}
+
+func TestCreateLogsTable(t *testing.T) {
+	// check database for the table
+
+	cfg := Config{
+		DatabaseFilename: "./testdata/test_database.db",
+		LogsTableName:    "logs",
+	}
+
+	db, err := cfg.BuildDB()
+	if err != nil {
+		t.Errorf("failed to build database, database filename: %s, logs table name: %s, err: %v",
+			cfg.DatabaseFilename, cfg.LogsTableName, err)
+	}
+
+	err = CreateLogsTable(context.Background(), &cfg, db)
+	if err != nil {
+		t.Errorf("failed to create logs table, database filename: %s, logs table name: %s, err: %v",
+			cfg.DatabaseFilename, cfg.LogsTableName, err)
+	}
+
+	_, err = db.ExecContext(context.Background(), fmt.Sprintf("SELECT ID FROM %s;", cfg.LogsTableName))
+	if err != nil {
+		t.Errorf("failed to select ID column database filename: %s, logs table name: %s, err: %v",
+			cfg.DatabaseFilename, cfg.LogsTableName, err)
+	}
+}
